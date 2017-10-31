1b849c82409009dd7dd666a4c028093ce460599a
blake reynolds <reynoldsa702@live.com>
2017-10-31 11:12:16 -0700

    add video method to make youtube embed urls

--- tests.py
+++ tests.py
@@ -7,14 +7,47 @@ class testVideo(unittest.TestCase):
     def test_create_Video(self):
         with self.assertRaises(TypeError):
             v = video.Video()
-
+        with self.assertRaises(TypeError):
+            v = video.Video(img="img", video_url="url")
+        with self.assertRaises(TypeError):
+            v = video.Video(title="movie", video_url="url")
+        with self.assertRaises(TypeError):
+            v = video.Video(title="movie", img="img")
         v = video.Video(title="movie", img="img", video_url="url")
 
     def test_Video_attributes(self):
         v = video.Video(title="movie", img="img", video_url="url")
         self.assertEqual(v.title, "movie")
         self.assertEqual(v.img, "img")
-        self.assertEqual(v.video_url, "")
+        self.assertEqual(v.video_url, "url")
+
+    def test_Video_embed_url(self):
+        v = video.Video(title="movie", img="img", video_url="url")
+        embedded = v.embed_url()
+        self.assertEqual(embedded, "")
+        v = video.Video(
+                title="movie",
+                img="img",
+                video_url="https://www.youtu.be/LQRawYZl-ls"
+        )
+
+        embedded = v.embed_url()
+        self.assertEqual(
+                embedded,
+                "https://www.youtube.com/embed/LQRawYZl-ls"
+        )
+
+        v = video.Video(
+                title="movie",
+                img="img",
+                video_url="https://www.youtube.com/watch?v=LQRawYZl-ls"
+        )
+
+        embedded = v.embed_url()
+        self.assertEqual(
+                embedded,
+                "https://www.youtube.com/embed/LQRawYZl-ls"
+        )
 
     def test_video_subclass(self):
         pass
--- video.py
+++ video.py
@@ -1,5 +1,26 @@
+from urllib.parse import urlsplit
+
 class Video:
+
+    youtube_domains = (
+        'www.youtu.be',
+        'www.youtube.com'
+    )
+
+    youtube_embed_url = 'https://www.youtube.com/embed/{}'
+
     def __init__(self, title, img, video_url):
         self.title = title
         self.img = img
-        self.video_url = ''
+        self.video_url = video_url
+
+    def embed_url(self):
+        split_url = urlsplit(self.video_url)
+        if split_url.netloc in self.youtube_domains:
+            if split_url.query:
+                id = split_url.query[2:]
+            else:
+                id = split_url.path[1:]
+            return self.youtube_embed_url.format(id)
+        else:
+            return ''
