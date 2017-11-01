da0fbd748b47c3e80f47193604cb2528bcb79917
blake reynolds <reynoldsa702@live.com>
2017-10-31 20:13:45 -0700

    change video class to reformat video url before assignment

--- video.py
+++ video.py
@@ -12,10 +12,10 @@ class Video:
     def __init__(self, title, img, video_url):
         self.title = title
         self.img = img
-        self.video_url = video_url
+        self.video_url = self.embed_url(video_url)
 
-    def embed_url(self):
-        split_url = urlsplit(self.video_url)
+    def embed_url(self, video_url):
+        split_url = urlsplit(video_url)
         if split_url.netloc in self.youtube_domains:
             if split_url.query:
                 id = split_url.query[2:]
