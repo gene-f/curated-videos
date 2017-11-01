ad56c6c1afc2398fc14ea899f413109552a3dd9e
blake reynolds <reynoldsa702@live.com>
2017-10-31 20:14:21 -0700

    add create_section function to create html for catergorized videos

--- create_page.py
+++ create_page.py
@@ -7,17 +7,44 @@ def group_by_type(*args):
             groups[type(arg)].append(arg)
     return groups
 
+
 def create_entry_template(p):
-    with open('html_templates/entry.html', 'r') as f:
-        base = f.read()
-    template = '<ul>\n'
+    base = (
+        '<article>'
+        '   <h3>{title}</h3>'
+        '   <img src="{img}"></img>'
+        '   <iframe src="{video_url}"></iframe>'
+        '   <p>'
+        '       %s'
+        '   </p>'
+        '</article>'
+    )
+
+    inner = '\n<ul>\n'
     fields = vars(p).keys()
     for field in fields:
         if field not in ('title', 'img', 'video_url'):
-            template += (
-                '           <li> <strong>%s:</strong> {%s}</li>\n'
+            inner += (
+                '<li> <strong>%s:</strong> {%s}</li>\n'
                 % (field, field)
             )
-    template += '       </ul>'
-    template = base % template
+    inner += '</ul>\n'
+    template = base % inner
     return template
+
+
+def create_section(key, videos):
+    base = (
+        '<section>'
+        '   <h2>{category}</h2>'
+        '   {content}'
+        '</section>'
+    )
+
+    content = ''
+    entry_template = create_entry_template(videos[0])
+    for video in videos:
+        content += entry_template.format(**vars(video)) + '\n'
+
+    section = base.format(category, content)
+    return section
