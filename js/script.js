7193d294b627ed205195002e1d6edd3eb2572e6e
blake reynolds <reynoldsa702@live.com>
2017-11-01 17:51:49 -0700

    write toggle function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 function toggle(button) {
     var parent_container = button.parentElement;
-    console.log(parent_container);
+    var iframe = parent_container.getElementsByTagName("iframe")[0]
+    var img = parent_container.getElementsByTagName("img")[0]
+    var p = parent_container.getElementsByTagName("p")[0]
+    
+    img.style.display = "none";
+    p.style.display = "none";
+    iframe.style.display = "block";
 }
